e617f68a1004bfa5901f11ba630ed5b822ce1fdb
Ryan McGreevy <ryanmcgreevy@gmail.com>
2018-08-14 16:19:31 -0400

    changed sim to voltool

--- mdff_sim.tcl
+++ mdff_sim.tcl
@@ -31,19 +31,21 @@ namespace eval ::MDFF::Sim:: {
 
   variable defaultGridspacing 1.0
   variable defaultTargetResolution 10.0
-
+  variable defaultCCDeprecate 0
 }
 
 proc ::MDFF::Sim::mdff_sim_usage { } {
 
   variable defaultTargetResolution
   variable defaultGridspacing
+  variable defaultCCDeprecate 
 
   puts "Usage: mdff sim <atomselection> -o <output map> ?options?"
   puts "Options:"
   puts "  -res <target resolution in Angstroms> (default: $defaultTargetResolution)"
-  puts "  -spacing <grid spacing in Angstroms> (default: $defaultGridspacing)"
+  puts "  -spacing <grid spacing in Angstroms> (default based on res, otherwise if using -deprecate: $defaultGridspacing)"
   puts "  -allframes (average over all frames)"
+  puts "  -deprecate <use the older, slower correlation algorithm (ccc only)> (on: 1 off: 0, default:$defaultCCDeprecate)"
 
 }
 
@@ -51,6 +53,7 @@ proc ::MDFF::Sim::mdff_sim { args } {
 
   variable defaultGridspacing
   variable defaultTargetResolution
+  variable defaultCCDeprecate
 
   set nargs [llength $args]
   if {$nargs == 0} {
@@ -77,19 +80,28 @@ proc ::MDFF::Sim::mdff_sim { args } {
       -o     { set arg(o)     $val }
       -res   { set arg(res)   $val }
       -spacing { set arg(spacing) $val }
+      -deprecate { set arg(deprecate) $val }
     }
   }
 
   if { [info exists arg(spacing)] } {
     set gridspacing $arg(spacing)
+    set use_gridspacingdefault 0
   } else {
     set gridspacing $defaultGridspacing
+    set use_gridspacingdefault 1
   }
 
   if { [info exists arg(res)] } {
     set targetResolution $arg(res)
   } else {
     set targetResolution $defaultTargetResolution
+  }
+  
+  if { [info exists arg(deprecate)] } {
+    set deprecate $arg(deprecate)
+  } else {
+    set deprecate $defaultCCDeprecate
   }
 
   if { [info exists arg(o)] } {
@@ -98,27 +110,32 @@ proc ::MDFF::Sim::mdff_sim { args } {
     error "Missing output dx map."
   }
 
-  # Interpolate atomic numbers onto a map
-  set weight [::MDFF::Tmp::getAtomicNumber $sel]
+  if $deprecate {
+    # Interpolate atomic numbers onto a map
+    set weight [::MDFF::Tmp::getAtomicNumber $sel]
 
-  # Get temporary filename
-  set tmpDir [::MDFF::Tmp::tmpdir]
-  set tmpDX [file join $tmpDir \
-    [::MDFF::Tmp::tmpfilename -prefix mdff_sim -suffix .dx -tmpdir $tmpDir]]
+    # Get temporary filename
+    set tmpDir [::MDFF::Tmp::tmpdir]
+    set tmpDX [file join $tmpDir \
+      [::MDFF::Tmp::tmpfilename -prefix mdff_sim -suffix .dx -tmpdir $tmpDir]]
 
-  if $allFrames {
-    volmap interp $sel -res $gridspacing -weight $weight -o $tmpDX -allframes
+    if $allFrames {
+      volmap interp $sel -res $gridspacing -weight $weight -o $tmpDX -allframes
+    } else {
+      volmap interp $sel -res $gridspacing -weight $weight -o $tmpDX
+    }
+
+    # Low pass filter to the target resolution 
+    # volutil pre-divides sigma by sqrt(3), so we don't have to do it here
+    set sigma [expr {0.5 * $targetResolution}]
+    ::VolUtil::volutil -o $dxout -pad -smooth $sigma $tmpDX
+
+    file delete $tmpDX
+  } elseif $use_gridspacingdefault {
+    voltool sim $sel -res $targetResolution -o $dxout 
   } else {
-    volmap interp $sel -res $gridspacing -weight $weight -o $tmpDX
+    voltool sim $sel -res $targetResolution -spacing $gridspacing -o $dxout 
   }
-
-  # Low pass filter to the target resolution 
-  # volutil pre-divides sigma by sqrt(3), so we don't have to do it here
-  set sigma [expr {0.5 * $targetResolution}]
-  ::VolUtil::volutil -o $dxout -pad -smooth $sigma $tmpDX
-
-  file delete $tmpDX
-
   return
 
 }
