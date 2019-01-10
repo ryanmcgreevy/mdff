a121080571f10cac553d124b71a0687ba23e4ce7
Ryan McGreevy <ryanmcgreevy@gmail.com>
2019-01-10 11:10:38 -0500

    removed -deprecate options from sim and cc. mdff plugin is now entirely independent from volutil

--- mdff_sim.tcl
+++ mdff_sim.tcl
@@ -11,7 +11,7 @@
 #
 #       $RCSfile: mdff_sim.tcl,v $
 #       $Author: ryanmcgreevy $        $Locker:  $             $State: Exp $
-#       $Revision: 1.2 $       $Date: 2018/09/12 17:39:39 $
+#       $Revision: 1.3 $       $Date: 2019/01/10 16:06:22 $
 #
 ############################################################################
 
